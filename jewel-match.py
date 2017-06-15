62bebae4c35ef49261ce27635995c6513c968c00
CSCannon <Christopher-Cannon@users.noreply.github.com>
2017-06-15 08:49:25 +0100

    Added fill_empty() function

--- jewel-match.py
+++ jewel-match.py
@@ -1,10 +1,23 @@
 import random
 
+# Create and populate jewel grid
 def create_playfield(width, height):
     jewels = ['X', 'O', '#', '@']
 
     return [[random.choice(jewels) for x in range(width)] for y in range(height)]
 
+# Find any blank spots and fill with jewels
+def fill_empty(playfield):
+    jewels = ['X', 'O', '#', '@']
+
+    for row in playfield:
+        for x in range(len(row)):
+            if(row[x] == ''):
+                row[x] = random.choice(jewels)
+
+    return playfield
+
+# Output the playfield
 def display_playfield(playfield):
     for row in playfield:
         for elem in row:
