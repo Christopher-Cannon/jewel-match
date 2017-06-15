1e931fcc3c688e3e8e87cd1ee5c3f701dbc3a575
CSCannon <Christopher-Cannon@users.noreply.github.com>
2017-06-15 12:03:48 +0100

    Added jewel swapping

--- jewel-match.py
+++ jewel-match.py
@@ -2,17 +2,17 @@ import random
 
 # Create and populate jewel grid
 def create_playfield(width, height):
-    jewels = ['X', 'O', '#', '@']
+    jewels = ['X', 'O', '#', '@', '%']
 
     return [[random.choice(jewels) for x in range(width)] for y in range(height)]
 
 # Find any blank spots and fill with jewels
 def fill_empty(playfield):
-    jewels = ['X', 'O', '#', '@']
+    jewels = ['X', 'O', '#', '@', '%']
 
     for row in playfield:
         for x in range(len(row)):
-            if(row[x] == ''):
+            if(row[x] == ' '):
                 row[x] = random.choice(jewels)
 
     return playfield
@@ -30,15 +30,55 @@ def remove_jewel(playfield):
     x = int(input("X to remove: "))
     y = int(input("Y to remove: "))
 
-    playfield[y][x] = ''
+    playfield[y][x] = ' '
 
     return playfield
+
+# Swap a jewel with either its left or right neighbour
+def swap_jewels(playfield):
+    while(True):
+        try:
+            x = int(input("X of jewel to swap: "))
+            y = int(input("Y of jewel to swap: "))
+        except:
+            print("Invalid input, please try again")
+            continue
+
+        # If there is space on either side of chosen jewel, we can swap either side
+        if(0 < x < len(playfield) - 1):
+            while(True):
+                try:
+                    choice = input("Which direction to swap, left or right? ")
+                except:
+                    print("Invalid input, please try again")
+                    continue
+
+                if(choice.lower() == "right"):
+                    print("Swapping right...\n")
+                    playfield[y][x], playfield[y][x + 1] = playfield[y][x + 1], playfield[y][x]
+                elif(choice.lower() == "left"):
+                    print("Swapping left...\n")
+                    playfield[y][x], playfield[y][x - 1] = playfield[y][x - 1], playfield[y][x]
+                else:
+                    print("Invalid input, please try again")
+                    continue
+
+                return playfield
+
+        elif(x == 0):
+            print("Swapping right...\n")
+            playfield[y][x], playfield[y][x + 1] = playfield[y][x + 1], playfield[y][x]
+        else:
+            print("Swapping left...\n")
+            playfield[y][x], playfield[y][x - 1] = playfield[y][x - 1], playfield[y][x]
+
+        return playfield
 
 # If a jewel is removed, move above jewels down one
 def check_field(playfield):
     for y in range(len(playfield)):
         for x in range(len(playfield[y])):
-            if(playfield[y][x] == ''):
+            if(playfield[y][x] == ' '):
                 if(y > 0):
                     pos = y
                     # Move jewels down and empty space up
@@ -54,8 +94,5 @@ def check_field(playfield):
 playfield = create_playfield(8, 8)
 display_playfield(playfield)
 
-playfield = remove_jewel(playfield)
-display_playfield(playfield)
-
-check_field(playfield)
+playfield = swap_jewels(playfield)
 display_playfield(playfield)
