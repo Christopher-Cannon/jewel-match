8f61edbe0ad0b974d71b6d8191beeb8d7f71fdc9
CSCannon <Christopher-Cannon@users.noreply.github.com>
2017-06-18 13:42:30 +0100

    Added game loop

--- jewel-match.py
+++ jewel-match.py
@@ -52,8 +52,17 @@ def remove_jewels(playfield, remove_list):
 def swap_jewels(playfield):
     while(True):
         try:
+            # Allow user to start at 1 rather than 0
+            print("Range: 1 - {}".format(len(playfield)))
             x = int(input("X of jewel to swap: "))
             y = int(input("Y of jewel to swap: "))
+
+            x -= 1
+            y -= 1
+
+            if(not(0 < x < len(playfield)) or not(0 < y < len(playfield))):
+                print("Invalid input, please try again")
+                continue
         except:
             print("Invalid input, please try again")
             continue
@@ -165,21 +174,55 @@ def prepare_field(playfield, destroy_list):
 
     return playfield
 
+# Main loop
 def main():
-    moves = 5
+    moves, score = 5, 0
     size = get_size()
     playfield = create_playfield(size)
 
     destroy_list = find_matches(playfield)
-    print(destroy_list)
-
+    # Ensure that playfield has no combos before starting
     while(destroy_list != []):
-        print(destroy_list == [])
         playfield = prepare_field(playfield, destroy_list)
-
         destroy_list = find_matches(playfield)
-        print(destroy_list)
 
     display_playfield(playfield)
+    # Continue while player has moves remaining
+    while(moves > 0):
+        jewels_marked, combo = 0, 0
+        print("Moves remaining: {}".format(moves))
+        print("Current score: {}".format(score))
+
+        playfield = swap_jewels(playfield)
+        moves -= 1
+
+        destroy_list = find_matches(playfield)
+        jewels_marked = len(destroy_list)
+
+        # Check for matches
+        while(destroy_list != []):
+            combo += 1
+
+            playfield = prepare_field(playfield, destroy_list)
+            destroy_list = find_matches(playfield)
+
+        if(jewels_marked > 0) and (combo > 0):
+            score += (jewels_marked * combo) * 10
+            moves += 1 * ((combo // 2) + 1)
+            
+            print("{} combo!".format(combo))
+            print("+ {} score".format((jewels_marked * combo) * 10))
+        elif(jewels_marked > 0) and (combo == 0):
+            score += jewels_marked * 10
+            moves += 1
+
+            print("+ {} score".format(jewels_marked * 10))
+        else:
+            pass
+
+        display_playfield(playfield)
+
+    print("Out of moves")
+    print("Final score: {}".format(score))
 
 main()
